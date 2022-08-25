2e1472cd0cbcf1e143f8d8d424e6a514f73b75be
Leonardo C <leonardocoel98@gmail.com>
2022-08-24 21:32:39 -0300

    feat: create matches migration and model

--- app/backend/src/database/migrations/20220824190607-create-teams.js
+++ app/backend/src/database/migrations/20220824190607-create-teams.js
@@ -23,6 +23,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-   await queryInterface.dropTable("teams")
+    await queryInterface.dropTable("teams");
   },
 };
---
+++ app/backend/src/database/models/matches.model.ts
@@ -0,0 +1,66 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Teams from './teams.model';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam' });
+
+export default Matches;
